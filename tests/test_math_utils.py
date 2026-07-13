f4becc235ad74b5f05bc9f7e7b9060a9e628a7de
Teo Wei Han <dextertwh@gmail.com>
2026-07-14 00:40:12 +0800

    1

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,5 +1,9 @@
-from src.math_utils import add
+import sys
+from pathlib import Path
 
+sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
+
+from math_utils import add
 
 def test_add():
     assert add(2, 3) == 5
